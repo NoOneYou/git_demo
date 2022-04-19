18032658146f15b661b5dbf1ccb60f8c4b7b7e7d
于林涛 <843495026@qq.com>
2022-04-19 23:47:17 +0800

    1.master commit

--- src/main/java/com/atguigu/test/HelloGit.java
+++ src/main/java/com/atguigu/test/HelloGit.java
@@ -2,6 +2,10 @@ package com.atguigu.test;
 
 public class HelloGit {
     public static void main(String[] args) {
+        //编写于master分支
         System.out.println("writing in master");
+        //编写于hotfix分支
+        System.out.println("writing in hotfix");
+        //
     }
 }
